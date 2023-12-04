2993fe740851986e38222419f68ce93cd6062032
edwardrico <120380332+edwardrico@users.noreply.github.com>
2023-12-04 19:37:32 +0100

    new

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -23,7 +23,7 @@ const Header = () => {
             <div className="header-text">
             <p> À la recherche active d'une alternance dès javier 2024. Durée <br></br>
              de 1 an avec un rythme de 4 jours à l'entreprise et 1 jour à l'école. <br></br>
-             après une immersion totale en tant que concepteur d'applications web<br></br>
+             Après une immersion totale en tant que concepteur d'applications web<br></br>
              et mobiles à l'école Ada Tech School, je suis prêt à mettre en pratique<br></br> 
              toutes mes compétences fraîchement acquises. Mon portfolio contient<br></br>
              toutes les informations essentielles pour m'aider à décrocher cette <br></br>
@@ -41,7 +41,7 @@ const Header = () => {
             <a href="#contact" className="scroll_down">Descendre</a>
           
           </div>
-          
+         
        </header>
         </section>
     );
