0724b2843283e25164f955ef11e3706cc3a8e0f5
edwardrico <120380332+edwardrico@users.noreply.github.com>
2024-02-09 15:09:37 +0100

    project

--- src/components/projets/Project.1.jsx
+++ src/components/projets/Project.1.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./project.css";
 import pico from "../../assets/pico-8.jpg";
 import api from "../../assets/travel-time.jpg";
 import php from "../../assets/php.jpg";
@@ -9,7 +8,7 @@ import meteo from "../../assets/MetoApi.png";
 import ProjectNav from "./ProjectNav";
 import { FaGithub } from "react-icons/fa";
 
-const Project = () => {
+export const Project = () => {
   return (
     <section id="project">
       <h1>Projets</h1>
@@ -22,8 +21,7 @@ const Project = () => {
             <a
               href="https://github.com/edwardrico/SpotMyTown-"
               className="btn"
-              target="_blank"
-              rel="noreferrer"
+              target="_blank" rel="noreferrer"
             >
               <FaGithub />
             </a>
@@ -33,12 +31,11 @@ const Project = () => {
         <article className="project-item">
           <div className="project-item-img">
             <img src={meteo} alt="MeteoApi" />
-            <h3>Meteo Api</h3>
+            <h3>SpotMyTown</h3>
             <a
               href="https://github.com/edwardrico/App-Meteo-API"
               className="btn"
-              target="_blank"
-              rel="noreferrer"
+              target="_blank" rel="noreferrer"
             >
               <FaGithub />
             </a>
@@ -52,8 +49,7 @@ const Project = () => {
             <a
               href="https://github.com/edwardrico/projet-collectif---pico8-les-mousquetaires"
               className="btn"
-              target="_blank"
-              rel="noreferrer"
+              target="_blank" rel="noreferrer"
             >
               <FaGithub />
             </a>
@@ -66,8 +62,7 @@ const Project = () => {
             <a
               href="https://github.com/edwardrico/projet-collectif---dataviz-api-thebestgroup"
               className="btn"
-              target="_blank"
-              rel="noreferrer"
+              target="_blank" rel="noreferrer"
             >
               <FaGithub />
             </a>
@@ -80,8 +75,7 @@ const Project = () => {
             <a
               href="https://github.com/edwardrico/projet-collectif---reseau-social-php-team_agathe_edward_francois"
               className="btn"
-              target="_blank"
-              rel="noreferrer"
+              target="_blank" rel="noreferrer"
             >
               <FaGithub />
             </a>
@@ -94,8 +88,7 @@ const Project = () => {
             <a
               href="https://github.com/edwardrico/projet-collectif---plateforme-de-vente-de-meubles-meublea"
               className="btn"
-              target="_blank"
-              rel="noreferrer"
+              target="_blank" rel="noreferrer"
             >
               <FaGithub />
             </a>
@@ -105,5 +98,3 @@ const Project = () => {
     </section>
   );
 };
-
-export default Project;
